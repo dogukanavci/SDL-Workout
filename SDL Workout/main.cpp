96e6ad089d3f4a7c31a789979abd773e319622a8
dogukanavci <37452679+dogukanavci@users.noreply.github.com>
2018-07-31 17:39:38 +0300

    cleaned unnecessary assets and functions

--- SDL Workout/main.cpp
+++ SDL Workout/main.cpp
@@ -4,7 +4,6 @@
 
 #include "Game.hpp"
 using namespace std;
-//int getImage();
 
 Game *game=nullptr;
 int main(int argc, const char * argv[]) {
@@ -29,50 +28,4 @@ int main(int argc, const char * argv[]) {
     game->clean();
     return 0;
 }
-
-
-int getImage(){
-    const int WIDTH=800;
-    const int HEIGHT=600;
-    SDL_Surface *imageSurface=NULL;
-    SDL_Surface *windowSurface=NULL;
-    
-    if(SDL_Init( SDL_INIT_EVERYTHING)<0){
-        cout<<"SDL could not be started"<<SDL_GetError()<<endl;
-    }
-    SDL_Window *window=SDL_CreateWindow("Hello SDL", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, SDL_WINDOW_ALLOW_HIGHDPI);
-    windowSurface=SDL_GetWindowSurface(window);
-    
-    if(window==NULL){
-        cout<<"Could not open the window "<<endl;
-        return EXIT_FAILURE;
-    }
-    if(!(IMG_Init(IMG_INIT_PNG) && IMG_INIT_PNG)){
-        cout<<"could not initialize"<<IMG_GetError()<<endl;
-        return 1;
-    }
-    SDL_Event windowEvent;
-    imageSurface=IMG_Load("/Users/dogukanavci/Desktop/summerTraining/summerTraining/SDL Workout/Assets/SMan.png");
-    if(imageSurface==NULL){
-        cout<<"could not load image"<<SDL_GetError() <<endl;
-    }
-    
-    while(true){
-        if(SDL_PollEvent(&windowEvent)){
-            if(SDL_QUIT==windowEvent.type){
-                break;
-            }
-        }
-        SDL_BlitSurface(imageSurface,NULL,windowSurface,NULL);
-        SDL_UpdateWindowSurface(window);
-    }
-    SDL_FreeSurface(imageSurface);
-    SDL_FreeSurface(windowSurface);
-    imageSurface=NULL;
-    windowSurface=NULL;
-    SDL_DestroyWindow(window);
-    SDL_Quit();
-    return 0;
-}
-
 
